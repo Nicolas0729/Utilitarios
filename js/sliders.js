d9cc01cee000dc58cb62a8eb7800375e7a854787
Nicolas0729 <107268101+Nicolas0729@users.noreply.github.com>
2023-01-06 19:09:10 -0500

    home utilitarios

---
+++ js/sliders.js
@@ -0,0 +1,25 @@
+$('.slider_cover').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout:3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        300:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 1,
+            nav: false
+        },
+        1140:{
+            items: 1,
+            nav: false
+        }
+        
+    }    
+})

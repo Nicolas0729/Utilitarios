b2a404fa9ac668d13f68c11e618da5771a179739
Nicolas0729 <107268101+Nicolas0729@users.noreply.github.com>
2023-01-20 15:26:13 -0500

    cambios

--- js/sliders.js
+++ js/sliders.js
@@ -12,12 +12,117 @@ $('.slider_cover').owlCarousel({
             items: 1,
             nav: false
         },
+        424:{
+            items: 1,
+            nav: false
+        },
         600:{
             items: 1,
             nav: false
         },
         1140:{
             items: 1,
+            nav: false
+        }
+        
+    }    
+})
+
+$('.carousel_brands').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout:3000,
+    margin: 20,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        424:{
+            items: 1.5,
+            nav: false
+        },
+        600:{
+            items: 1,
+            nav: false
+        },
+        900:{
+            items: 4,
+            nav: false
+        },
+        1140:{
+            items: 5,
+            nav: false
+        },
+        1340:{
+            items: 5,
+            nav: false
+        }
+        
+    }     
+})
+
+$('.carousel_motorcycle').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin: 20,
+    autoplayTimeout:3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        300:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 1,
+            nav: false
+        },
+        900:{
+            items: 1,
+            nav: false
+        },
+        1140:{
+            items: 1,
+            nav: false
+        },
+        1340:{
+            items: 1,
+            nav: false
+        }
+        
+    }    
+})
+
+$('.carousel_logos').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin: 20,
+    autoplayTimeout:3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        424:{
+            items: 2,
+            nav: false
+        },
+        600:{
+            items: 4,
+            nav: false
+        },
+        1000:{
+            items: 6,
+            nav: false
+        },
+        1340:{
+            items: 7,
             nav: false
         }
         
